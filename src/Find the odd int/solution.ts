ecef8567b566f21b40b7ce040c048160f68b7293
Ilya Tovpeko <tovpekoIlya@gmail.com>
2022-10-21 10:11:28 +0300

    added 'find the odd int' solution

---
+++ src/Find the odd int/solution.ts
@@ -0,0 +1,30 @@
+function solve(array: number[]): number{
+    return findOdd(array);
+}
+
+function findOdd(array: number[]): number{
+    let map: Array<Array<number>> = [[], []];
+    let result: number = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if(!map[0].includes(array[i])){
+            map[0].push(array[i]);
+            map[1].push(1);
+        }
+        else{
+            let index = map[0].indexOf(array[i]);
+            map[1][index] += 1;
+        }
+    }
+
+    for (let i = 0; i < map[1].length; i++) {
+        if(map[1][i] % 2 != 0){
+            result = map[0][i];
+            break;
+        }
+    }
+
+    return result;
+}
+
+export {solve};
