dbca05c054760e657c7107887a8d7943f7c3defc
Ilya Tovpeko <tovpekoIlya@gmail.com>
2022-10-23 20:23:14 +0300

    edited 5-async-functions-explained.js

--- src/Asynchrony/5-async-functions-explained.js
+++ src/Asynchrony/5-async-functions-explained.js
@@ -1,9 +1,9 @@
 // TASK: Попробуйте изменять переменные и понаблюдать, что получится.
 // Мое понимание: 1) при secure == false, отклоняем выполнение промиса функцией reject и
-// выбрасываем ошибку в функции sendData, котороя потом отлавливается в цепочке
+// выбрасываем ошибку в функции sendData, которая потом отлавливается в цепочке
 //                2) при sendSuccessfully == false, отклоняем выполнение промиса функцией reject и
-// выбрасываем ошибку в функции sendData, котороя потом отлавливается в цепочке
-//                3) при secure, sendSuccessfully = true - все ок, 
+// выбрасываем ошибку в функции sendData, которая потом отлавливается в цепочке
+//                3) при secure, sendSuccessfully == true - все ок,
 const secure = true;
 const sendSuccessfully = true;
 
@@ -44,7 +44,7 @@ function sendData(connection, data) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (!connection.secure) {
-        return reject(new Error("Can't send data with insecure connection"));
+        return reject(new Error('Can\'t send data with insecure connection'));
       }
       if (!connection.send(data)) {
         return reject(new Error('Error during sending'));
@@ -54,41 +54,10 @@ function sendData(connection, data) {
   });
 }
 
-// createConnection()
-//   .then((connection) => {
-//     const data = {
-//       status: 200,
-//       message: 'Hello, mister!',
-//       prepared: false,
-//     };
-//     // думаю тут тоже можно это отметить:
-//     // 1-ый уровень вложенности
-//     const promise = prepareData(data, connection)
-//       // 2-ой уровень вложенности
-//       .then((prepared) => connection.serializeData(prepared))
-//       .then((serialized) => {
-//         return {
-//           data: serialized,
-//           connection,
-//         };
-//       });
-
-//     return promise;
-//   })
-//   .then((result) => {
-//     const { data, connection } = result;
-//     return sendData(connection, data);
-//   })
-//   .then(() => {
-//     console.log('Sent');
-//   })
-//   .catch(console.error);
-
-
 // Task 2
 // Вывод: с async/await код более лаконичный (его меньше => понятнее)
-async function main(){
-  try{
+async function main() {
+  try {
     const data = {
       status: 200,
       message: 'Hello, mister!',
@@ -97,12 +66,11 @@ async function main(){
 
     const connection = await createConnection();
     await prepareData(data, connection);
-    await sendData(connection, data)
+    await sendData(connection, data);
     console.log('Sent');
-  }
-  catch(error){
+  } catch (error) {
     console.log(error);
   }
 }
 
-  main();
+main();
